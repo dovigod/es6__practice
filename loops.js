d1accfb451ee0a8c2a26c77e18930b3886b8b64b
dovigod <inaki96@naver.com>
2020-06-10 19:16:26 +0900

    loops.js added

---
+++ loops.js
@@ -0,0 +1,11 @@
+const jisang = ['1','2','3'];
+
+
+
+
+for(let friend of jisang){
+    console.log(friend);
+}
+/// of  는 다른쪽으로 작용가능 , 비단 배열뿐만아니라, string에서도, obj에서도 가능 foreach는 배열에서만
+
+// 루프 멈출수도 있음
