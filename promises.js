ac011d00daee9632c94b81205f7417ec1077ae81
dovigod <inaki96@naver.com>
2020-06-11 01:50:16 +0900

    promise all~~~~

--- promises.js
+++ promises.js
@@ -18,6 +18,7 @@ console.log(hello);
 //비동기 작업이 맞이할 미래의 완료 혹은 실패와 그 결과값을 나타냄 
 //=> 실행할수 있는 함수 만들어야해 
 
+/*
 
 const hello = new Promise((resolve,reject)=>{
      setTimeout(()=>{
@@ -58,3 +59,36 @@ jisang.then(numb =>{
 }).then(last =>{
     console.log(last);
 }).catch(error => console.log(error)); //이러면 에러 객체가 발생하는 곳을 로그해줌
+
+
+*/
+
+
+
+///promise.all = => 주어진 모든 약속들이 실행된후 실행되는 하나의 promise 한개의 promise return
+
+
+const p1 = new Promise((resolve) =>{
+    setTimeout(resolve , 5000,"first");
+});
+
+const p2 = new Promise((resolve) =>{
+    setTimeout(reject , 1000,"fuck");
+}); 
+
+const p3 =new Promise((resolve,reject) =>{
+    setTimeout(resolve , 3000,"third");
+});
+
+//// 여러개의 api들을 받아올떄 사용할수 있ㅇㅇ
+
+
+const motherPromise = Promise.all([p1,p2,p3]);
+
+motherPromise.then(values =>{
+    console.log(values);
+}).catch(err =>console.log(err));
+
+//==> [ f,s,t] ==> 실행 시간에 상관없이 순서대로 array가 만들어짐
+
+//reject로 바꾸면  error발생,, 하나라도 거절당하면 마더약속도 거절당함 거절당하는 순간 바로 거절
